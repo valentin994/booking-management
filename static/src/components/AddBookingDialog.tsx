d1a49769676836fb59710d14adc8c98a42ca0ae1
valentin994 <valentin.vareskic@outlook.com>
2022-08-01 11:09:38 +0200

    redesigned the dialog

--- static/src/components/AddBookingDialog.tsx
+++ static/src/components/AddBookingDialog.tsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
 import {
-    Button,
-    Dialog,
-    DialogTitle,
-    DialogActions,
-    TextField,
-    Container,
-    Box,
-    InputLabel,
-    FormControl,
-    Input, MenuItem, Stack
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogActions,
+  TextField,
+  Container,
+  Box,
+  InputLabel,
+  FormControl,
+  Input,
+  MenuItem,
+  Stack,
 } from "@mui/material";
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
@@ -29,60 +31,71 @@ function AddBookingDialog() {
       <Button variant="outlined" onClick={handleClickOpen}>
         Add
       </Button>
-      <Dialog open={open} onClose={handleClose}>
-        <Box p={2}>
+      <Dialog open={open} onClose={handleClose} maxWidth="xs">
+        <Box p={4} pb={2}>
           <DialogTitle>Add Booking</DialogTitle>
           <Container>
-              <Stack spacing={2} p={0}>
-            <TextField
-              autoFocus
-              id="name"
-              label="Name"
-              type="text"
-              variant="standard"
-              fullWidth
-            />
-            <TextField
-              autoFocus
-              id="email"
-              label="Email"
-              type="email"
-              variant="standard"
-              fullWidth
-            />
-              </Stack>
-            <TextField
-              autoFocus
-              id="number"
-              label="Number"
-              inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
-              variant="standard"
-
-            />
-            <TextField autoFocus id="apartment" label="Apartment" select variant="standard">
+            <Stack spacing={2}>
+              <TextField
+                autoFocus
+                id="name"
+                label="Name"
+                type="text"
+                variant="standard"
+                fullWidth
+              />
+              <TextField
+                autoFocus
+                id="email"
+                label="Email"
+                type="email"
+                variant="standard"
+                fullWidth
+              />
+            </Stack>
+            <Stack direction="row" spacing={4} pt={2} pb={2}>
+              <TextField
+                autoFocus
+                id="number"
+                label="Number"
+                inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
+                variant="standard"
+                fullWidth
+              />
+              <TextField
+                autoFocus
+                id="apt"
+                label="Apt"
+                select
+                variant="standard"
+                sx={{ width: "20%" }}
+              >
                 <MenuItem>1</MenuItem>
                 <MenuItem>2</MenuItem>
-            </TextField>
-              <Stack direction="row" spacing={2}>
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
-              <DatePicker
-                label="Start Date"
-                value={startDate}
-                onChange={(newValue) => {
-                  setStartDate(newValue);
-                }}
-                renderInput={(params) => <TextField {...params} />}
-              />
-              <DatePicker
-                label="End Date"
-                value={endDate}
-                onChange={(newValue) => {
-                  setEndDate(newValue);
-                }}
-                renderInput={(params) => <TextField {...params} />}
-              />
-            </LocalizationProvider>
-              </Stack>
+              </TextField>
+            </Stack>
+            <Stack direction="row" spacing={2} pt={2} pb={2}>
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <DatePicker
+                  label="Start Date"
+                  value={startDate}
+                  onChange={(newValue) => {
+                    setStartDate(newValue);
+                  }}
+                  renderInput={(params) => <TextField {...params} />}
+                  inputFormat="dd/mm/yyyy"
+                />
+                <DatePicker
+                  label="End Date"
+                  value={endDate}
+                  onChange={(newValue) => {
+                    setEndDate(newValue);
+                  }}
+                  renderInput={(params) => <TextField {...params} />}
+                  inputFormat="dd/mm/yyyy"
+                />
+              </LocalizationProvider>
+            </Stack>
           </Container>
           <DialogActions>
             <Button onClick={handleClose}>Cancel</Button>
